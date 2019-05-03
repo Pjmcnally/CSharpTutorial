ba3db26a853d1107075608ffe249b5affdef93ca
Pjmcnally <Patrick.J.McNally@gmail.com>
2019-05-02 20:49:32 -0500

    Add clases tutorial.

---
+++ classes/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Patrick", 1000);
+            Console.WriteLine($"Account Created:\n\tAccount Number: {account.Number}\n\tAccount Holder: {account.Owner}\n\tAccount Balance: ${account.Balance:N2}");
+
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+
+            account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+            Console.WriteLine(account.Balance);
+            Console.Write(account.GetAccountHistory());
+            //try
+            //{
+            //    account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+            //}
+            //catch (InvalidOperationException e)
+            //{
+            //    Console.WriteLine("Exception caught trying to overdraw");
+            //    Console.WriteLine(e.ToString());
+            //}
+
+            //try
+            //{
+            //    var invalidAccount = new BankAccount("invalid", -55);
+            //}
+            //catch (ArgumentOutOfRangeException e)
+            //{
+            //    Console.WriteLine("Exception caught creating account with negative balance");
+            //    Console.WriteLine(e.ToString());
+            //}
+        }
+    }
+}
