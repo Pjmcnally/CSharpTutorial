6565fdef21c48fbffe80e1e377a988279bfd3209
Pjmcnally <Patrick.J.McNally@gmail.com>
2019-05-05 14:03:09 -0500

    Finish 204.2

---
+++ edX/DEV204.2/Mod1/Mod1_Lab1/Car.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod1_Lab1
+{
+    // Declaring the Car() Class
+    // This class has 3 properties: Color, Year, and Mileage
+    public class Car
+    {
+        // Creates integer variable called "instances" and assigns value to 0
+        private static int instances = 0;
+
+        // Defining Properties
+        public string Color { get; set; }
+        public int Year { get; set; }
+        public int Mileage { get; set; }
+
+        // Adding a constructor
+        public Car()
+        {
+            // Every time the constructor runs, increment "instances"
+            instances++;
+        }
+
+        // Adding another constructor
+        // This constructor instantiates a Car() object while only having the color and year information
+        public Car(string color, int year)
+        {
+            this.Color = color;
+            this.Year = year;
+            instances++;
+        }
+
+        // Adding another Constructor
+        // This constructor instantiates a Car() object while only having the car's year and mileage information available
+        public Car(int year, int mileage)
+        {
+            this.Year = year;
+            this.Mileage = mileage;
+            instances++;
+        }
+
+        // Declare static member
+        public static int CountCars()
+        {
+            return instances;
+        }
+
+        public override string ToString()
+        {
+            return $"This car is painted {this.Color}, was built in {this.Year}, and has {this.Mileage} miles on it.";
+        }
+    }
+}
---
+++ edX/DEV204.2/Mod1/Mod1_Lab1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod1_Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiating an object of Car() Class by using Type Inference called Car1
+            var Car1 = new Car();
+            var Car2 = new Car("Red", 2008);
+
+            // Using dot notation to call members on Car1
+            Car1.Color = "White";
+            Car1.Year = 2010;
+            Car1.Mileage = 11000;
+
+            // Output to the console window
+            Console.WriteLine(Car1);
+            Console.WriteLine(Car2);
+            Console.WriteLine($"There are {Car.CountCars()} cars on inventory right now.");
+        }
+    }
+}
